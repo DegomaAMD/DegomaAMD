7a605d8b031bb189996d142b478247ecdfa306d7
Andria Mae Degoma <degoma.andria@gmail.com>
2023-05-18 18:56:59 +0800

    first commit

--- Activity 4/scripts.js
+++ Activity 4/scripts.js
@@ -1,21 +1,4 @@
-const pres1 = document.getElementById("pres1");
-const pres1year = document.getElementById("pres1year");
-const pres2 = document.getElementById("pres2");
-const pres2year = document.getElementById("pres2year");
-const pres3 = document.getElementById("pres3");
-const pres3year = document.getElementById("pres3year");
-const pres4 = document.getElementById("pres4");
-const pres4year = document.getElementById("pres4year");
-const pres5 = document.getElementById("pres5");
-const pres5year = document.getElementById("pres5year");
-const pres6 = document.getElementById("pres6");
-const pres6year = document.getElementById("pres6year");
-const pres7 = document.getElementById("pres7");
-const pres7year = document.getElementById("pres7year");
-const pres8 = document.getElementById("pres8");
-const pres8year = document.getElementById("pres8year");
-const pres9 = document.getElementById("pres9");
-const pres9year = document.getElementById("pres9year");
+const act4Body = document.querySelector('#root');
 
 
 var presidents = [
@@ -57,23 +40,36 @@ var presidents = [
   },
 ];
 
-pres1.innerHTML = presidents[0].name;
-pres1year.innerHTML = presidents[0].details;
-pres2.innerHTML = presidents[1].name;
-pres2year.innerHTML = presidents[1].details;
-pres3.innerHTML = presidents[2].name;
-pres3year.innerHTML = presidents[2].details;
-pres4.innerHTML = presidents[3].name;
-pres4year.innerHTML = presidents[3].details;
-pres5.innerHTML = presidents[4].name;
-pres5year.innerHTML = presidents[4].details;
-pres6.innerHTML = presidents[5].name;
-pres6year.innerHTML = presidents[5].details;
-pres7.innerHTML = presidents[6].name;
-pres7year.innerHTML = presidents[6].details;
-pres8.innerHTML = presidents[7].name;
-pres8year.innerHTML = presidents[7].details;
-pres9.innerHTML = presidents[8].name;
-pres9year.innerHTML = presidents[8].details;
+const act4Image =  document.createElement('img');
+act4Body.appendChild(act4Image);
+
+act4Image.setAttribute('src', './logo.png');
+act4Image.setAttribute('alt', 'logo picture');
+
+const act4Div =  document.createElement('div');
+act4Body.appendChild(act4Div);
+act4Div.setAttribute('class', 'container');
 
 
+const Act4Cards = () => {
+  for (i = 0 ; i < presidents.length ; i++){ 
+    const act4Card =  document.createElement('div');
+    act4Div.appendChild(act4Card);
+    act4Card.setAttribute('class', 'card');
+
+    const act4CardPres = document.createElement('h1');
+    act4Card.appendChild(act4CardPres);
+    act4CardPres.setAttribute('class','card-title');
+    act4CardPres.innerHTML = presidents[i].name;
+
+    const act4CardYear = document.createElement('div');
+    act4Card.appendChild(act4CardYear);
+    act4CardYear.setAttribute('class','card-body');
+
+    const act4CardYearvalue = document.createElement('p');
+    act4CardYear.appendChild(act4CardYearvalue);
+    act4CardYearvalue.setAttribute('class','card-text');
+    act4CardYearvalue.innerHTML = presidents[i].details;
+  }
+}
+Act4Cards();
