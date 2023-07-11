9fdc267da02dcf534bc0751027f70ea27035aab6
Andria Mae Degoma <degoma.andria@gmail.com>
2023-07-11 19:17:36 +0800

    updated Capstone

--- MiniProject2/client/src/pages/Admin/Admin.js
+++ MiniProject2/client/src/pages/Admin/Admin.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Navbar from './components/navbar';
-import Offcanvas from './components/offcanvas';
 
 
 function Admin() {
   return (
     <div>
-      <Navbar />
-      <Offcanvas />
+
     </div>
   )
 }
